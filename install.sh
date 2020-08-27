379b02fc64ed658b790f5a668827b0492c6cf62e
de-wax <45847235+de-wax@users.noreply.github.com>
2020-08-27 15:35:10 +0200

    install.sh um aktuelle Go-Version angepasst

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
-#Installing Dependencies
-sudo apt install git go
+#Install git
+sudo apt install git
+#Install most actual go Version
+export GOPKG="$(curl -s https://api.github.com/repos/golang/go/git/matching-refs/tags/go | grep ref | grep -v url | grep -v beta | tail -1 | awk -F\/ {' print $3 '} | sed 's/",//')"
+wget https://golang.org/dl/$GOPKG.linux-armv6l.tar.gz
+sudo tar -C /usr/local -xzf $GOPKG.linux-armv6l.tar.gz
+rm $GOPKG.linux-armv6l.tar.gz
+echo PATH=$PATH:/usr/local/go/bin >> $HOME/.zshrc
+echo GOPATH=$HOME >> $HOME/.zshrc
+source $HOME/.zshrc
 #Make Folder and Clone Repository
 mkdir ventiPi && cd ventiPi
 git clone https://github.com/de-wax/ventiPi.git
