9d62c7edb04ed1faa17b659de35e7a5e7f7472a9
René <45847235+de-wax@users.noreply.github.com>
2020-08-31 09:50:43 +0200

    Update dewpoint.go

--- pkg/dewpoint/dewpoint.go
+++ pkg/dewpoint/dewpoint.go
@@ -1,9 +1,11 @@
 package dewpoint
 
 import (
-
+	"math"
 )
-
-func CalculateDewPoint() {
-	
+func Calculate(T float64, H float64) float64 {
+	const a float64 = 17.62
+	const b float64 = 243.12
+	alpha := math.Log(H/100) + a*T/(b+T)
+	return math.Round(((b*alpha)/(a-alpha))*100) / 100
 }
