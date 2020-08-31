deefe006cf03087c55051ac09d3890e643e62fbd
René <45847235+de-wax@users.noreply.github.com>
2020-08-31 11:09:03 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	# Standard Packages
+	// Standard Packages
 	"fmt"
 	"log"
 	
-	# Self-Written Packages
+	// Self-Written Packages
 	"github.com/de-wax/go-pkg/dewpoint"
 	
-	# Third-Party Packages
+	// Third-Party Packages
 )
 
 func main() {
+	// Only for testing the Package, with error-Handling
 	DP, err := dewpoint.Calculate(21.5, 56)
 	if err != nil {
 		log.Fatal(err)
